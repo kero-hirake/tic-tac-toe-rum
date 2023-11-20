1b1b5c3624c437d041ba441325bf717277305396
hirakero <k16.h.white@gmail.com>
2023-11-20 22:43:34 +0900

    props を通してデータを渡す

--- src/tic_tac_toe_rum/core.cljs
+++ src/tic_tac_toe_rum/core.cljs
@@ -3,23 +3,26 @@
             ["react" :as r]
             ["react-dom/client" :as rdom]))
 
-(defc square []
+(defc square [& {:keys [value]}]
+  [:div.square value])
+
+(defc board []
   [:*
    [:div.board-row 
-    [:div.square "1"]
-    [:div.square "2"]
-    [:div.square "3"]]
+    (square :value 1)
+    (square :value 2)
+    (square :value 3) ]
    [:div.board-row
-    [:div.square "4"]
-    [:div.square "5"]
-    [:div.square "6"]]
+    (square :value 4)
+    (square :value 5)
+    (square :value 6)]
    [:div.board-row
-    [:div.square "7"]
-    [:div.square "8"]
-    [:div.square "9"]]])
+    (square :value 7)
+    (square :value 8)
+    (square :value 9)]])
 
 (defc app []
-  (square))
+  (board))
 
 (defn init []
   ; FIX: react-dom.development.js:87 Warning: ReactDOM.render is no longer supported in React 18 
