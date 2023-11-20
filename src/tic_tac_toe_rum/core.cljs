eac2cc69d464820600f7d1d083d4969537dec0f2
hirakero <k16.h.white@gmail.com>
2023-11-20 22:31:08 +0900

    盤面の作成

--- src/tic_tac_toe_rum/core.cljs
+++ src/tic_tac_toe_rum/core.cljs
@@ -1,14 +1,28 @@
 (ns tic-tac-toe-rum.core
   (:require [rum.core :refer [defc mount]]
+            ["react" :as r]
             ["react-dom/client" :as rdom]))
 
-(defc label [text]
-  [:div.label text])
+(defc square []
+  [:*
+   [:div.board-row 
+    [:div.square "1"]
+    [:div.square "2"]
+    [:div.square "3"]]
+   [:div.board-row
+    [:div.square "4"]
+    [:div.square "5"]
+    [:div.square "6"]]
+   [:div.board-row
+    [:div.square "7"]
+    [:div.square "8"]
+    [:div.square "9"]]])
 
-(defn init [] 
-  ; FIX: react-dom.development.js:87 Warning: ReactDOM.render is no longer supported in React 18
-  ; FIX: react-dom.development.js:87 Warning: render(): Rendering components directly into document.
-  (mount (label "foo") js/document.body)  
+(defc app []
+  (square))
 
-  #_(println "hi!!"))
+(defn init []
+  ; FIX: react-dom.development.js:87 Warning: ReactDOM.render is no longer supported in React 18 
+  
+  (mount (app) (js/document.getElementById "app")))
 
