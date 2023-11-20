3c81c4a9570d8e2eefe9b68b61b5407b109e026d
hirakero <k16.h.white@gmail.com>
2023-11-20 22:14:08 +0900

    rum install

--- src/tic_tac_toe_rum/core.cljs
+++ src/tic_tac_toe_rum/core.cljs
@@ -1,5 +1,14 @@
-(ns tic-tac-toe-rum.core)
+(ns tic-tac-toe-rum.core
+  (:require [rum.core :refer [defc mount]]
+            ["react-dom/client" :as rdom]))
 
-(defn init []
-  (println "hello"))
+(defc label [text]
+  [:div.label text])
+
+(defn init [] 
+  ; FIX: react-dom.development.js:87 Warning: ReactDOM.render is no longer supported in React 18
+  ; FIX: react-dom.development.js:87 Warning: render(): Rendering components directly into document.
+  (mount (label "foo") js/document.body)  
+
+  #_(println "hi!!"))
 
