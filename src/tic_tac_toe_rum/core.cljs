8bfeb27dfb6a2e1816033450a7e793b86a3d177f
hirakero <k16.h.white@gmail.com>
2023-11-21 11:38:21 +0900

    state のリフトアップ

--- src/tic_tac_toe_rum/core.cljs
+++ src/tic_tac_toe_rum/core.cljs
@@ -3,39 +3,31 @@
             ["react" :as r]
             ["react-dom/client" :as rdom]))
 
-(defcs square < (local nil ::value) 
-  [state]  
-  (let [local-atom (::value state)
-        handle-click (fn [] (swap! local-atom (fn[_] "X")))]
-    [:div.square 
-     {:on-click handle-click} 
-     @local-atom]))
+(defc square  [& {:keys [value on-square-click]}]  
+  [:div.square
+   {:on-click on-square-click}
+   value])
 
-(defcs stateful < (local 0 ::key)
-  [state label]
-  (let [local-atom (::key state)]
-    [:div {:on-click (fn [_] (swap! local-atom inc))}
-     label ": " @local-atom]))
-
-(defc board []
-  [:*
-   [:div.board-row 
-    (square)
-    (square)
-    (square) ]
-   [:div.board-row
-    (square)
-    (square)
-    (square)]
-   [:div.board-row
-    (square)
-    (square)
-    (square)]])
-
+(defcs board < (local (vec (repeat 9 nil)) ::squares)
+  [{::keys [squares]}] 
+  (let [handle-click (fn [i]
+                       (println "clicked " (swap! squares assoc i "X")))]
+    [:*
+     [:div.board-row 
+      (square :value (get @squares 0) :on-square-click #(handle-click 0))
+      (square :value (get @squares 1) :on-square-click #(handle-click 1))
+      (square :value (get @squares 2) :on-square-click #(handle-click 2)) ]
+     [:div.board-row
+      (square :value (get @squares 3) :on-square-click #(handle-click 3))
+      (square :value (get @squares 4) :on-square-click #(handle-click 4))
+      (square :value (get @squares 5) :on-square-click #(handle-click 5))]
+     [:div.board-row
+      (square :value (get @squares 6) :on-square-click #(handle-click 6))
+      (square :value (get @squares 7) :on-square-click #(handle-click 7))
+      (square :value (get @squares 8) :on-square-click #(handle-click 8))]]))
 
 (defc app []
-  [:* 
-   (stateful "click count")
+  [:*  
    (board)])
 
 (defn init []
